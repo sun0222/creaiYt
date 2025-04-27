614a2ce3263b3f65726f00792a82e05a7ced2983
Sunil kumar <39265341+sun0222@users.noreply.github.com>
2025-04-27 16:35:03 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,5 +3,5 @@ from crewai_tools import YoutubeChannelSearchTool
 
 # Initialize the tool with a specific YouTube channel handle
 youtube_channel_tool = YoutubeChannelSearchTool(
-    youtube_channel_handle='@krishnaik06'
+    youtube_channel_handle='@CodeWithHarry'
 )
